6a32760c32f2dfe8e19413ccf0da9935d5b38fd9
Fazendaaa <lucas.carotta@outlook.com>
2022-02-27 13:11:35 -0300

    Adding breaking tests and moving to the correct test file

--- pkg/lexer_test.go
+++ pkg/lexer_test.go
@@ -1,1 +1,105 @@
 package shojo
+
+import (
+	"testing"
+
+	samael "github.com/Fazendaaa/Samael/pkg"
+)
+
+func TestLex(t *testing.T) {
+	t.Run("complete", func(t *testing.T) {
+		value, fail := samael.LexProject("shojo", "../test/config/complete/", projectFunc)
+
+		if nil != fail {
+			t.Errorf("got:\n%v\n and the given error condition is:\n%s", value, fail)
+		}
+	})
+
+	t.Run("default", func(t *testing.T) {
+		value, fail := samael.LexProject("shojo", "../test/config/default/", projectFunc)
+
+		if nil != fail {
+			t.Errorf("got:\n%v\n and the given error condition is:\n%s", value, fail)
+		}
+	})
+
+	t.Run("integer package", func(t *testing.T) {
+		value, fail := samael.LexProject("shojo", "../test/config/integerPackage/", projectFunc)
+
+		if nil == fail {
+			t.Errorf("Expected to get an error but got the response: \n%v", value)
+		}
+	})
+
+	t.Run("integer repository", func(t *testing.T) {
+		value, fail := samael.LexProject("shojo", "../test/config/integerRepository/", projectFunc)
+
+		if nil == fail {
+			t.Errorf("Expected to get an error but got the response: \n%v", value)
+		}
+	})
+
+	t.Run("integer Tex Version", func(t *testing.T) {
+		value, fail := samael.LexProject("shojo", "../test/config/integerTexVersion/", projectFunc)
+
+		if nil == fail {
+			t.Errorf("Expected to get an error but got the response: \n%v", value)
+		}
+	})
+
+	t.Run("missing packages", func(t *testing.T) {
+		value, fail := samael.LexProject("shojo", "../test/config/missingPackages/", projectFunc)
+
+		if nil == fail {
+			t.Errorf("Expected to get an error but got the response: \n%v", value)
+		}
+	})
+
+	t.Run("named", func(t *testing.T) {
+		value, fail := samael.LexProject("shojo", "../test/config/named/foo.yml", projectFunc)
+
+		if nil != fail {
+			t.Errorf("got:\n%v\n and the given error condition is:\n%s", value, fail)
+		}
+	})
+
+	t.Run("repository", func(t *testing.T) {
+		value, fail := samael.LexProject("shojo", "../test/config/repository/", projectFunc)
+
+		if nil != fail {
+			t.Errorf("got:\n%v\n and the given error condition is:\n%s", value, fail)
+		}
+	})
+
+	t.Run("string Tlmgr Version", func(t *testing.T) {
+		value, fail := samael.LexProject("shojo", "../test/config/stringTlmgrVersion/", projectFunc)
+
+		if nil == fail {
+			t.Errorf("Expected to get an error but got the response: \n%v", value)
+		}
+	})
+
+	t.Run("tex", func(t *testing.T) {
+		value, fail := samael.LexProject("shojo", "../test/config/tex/", projectFunc)
+
+		if nil != fail {
+			t.Errorf("got:\n%v\n and the given error condition is:\n%s", value, fail)
+		}
+	})
+
+	t.Run("tlmgr", func(t *testing.T) {
+		value, fail := samael.LexProject("shojo", "../test/config/tlmgr/", projectFunc)
+
+		if nil != fail {
+			t.Errorf("got:\n%v\n and the given error condition is:\n%s", value, fail)
+		}
+	})
+
+	t.Run("yml", func(t *testing.T) {
+		value, fail := samael.LexProject("shojo", "../test/config/yml/", projectFunc)
+
+		if nil != fail {
+			t.Errorf("got:\n%v\n and the given error condition is:\n%s", value, fail)
+		}
+	})
+}
